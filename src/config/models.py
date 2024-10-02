bf73af7451fd709a327833a78f0f5f3d134a033c
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2024-10-01 21:41:54 -0700

    ENH #10: Create models configurations setup file

---
+++ src/config/models.py
@@ -0,0 +1,10 @@
+ModelConfigList = [
+    {
+        "name": "Llama3.1",
+        "model": "meta-llama/Llama-3.1-8B-Instruct"
+    },
+    {
+        "name": "Mistral",
+        "model": "mistralai/Mistral-7B-Instruct-v0.3"
+    }
+]
