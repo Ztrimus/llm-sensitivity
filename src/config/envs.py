7c52a52cadefa727be86d54101d77845f93dcb3b
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2024-10-03 16:03:15 -0700

    ENH #10: Add new configs

--- src/config/envs.py
+++ src/config/envs.py
@@ -2,4 +2,4 @@ from src.config import credentials
 
 MODELS_DIR = f'/scratch/{credentials.ASURITE_ID}/cache/llm-sensitivity/models'
 DATASETS_DIR = f'/scratch/{credentials.ASURITE_ID}/cache/llm-sensitivity/datasets'
-MAX_RESPONSE_LEN = 300
+MAX_RESPONSE_LEN = 256
--- src/config/models.py
+++ src/config/models.py
@@ -1,14 +1,18 @@
 Config = {
-    'llama3.1':{
-        "name": "Llama3.1",
+    'llama31':{
+        "name": "Llama31",
         "checkpoint": "meta-llama/Llama-3.1-8B-Instruct"
+    },
+    'mistral':{
+        "name": "Mistral",
+        "checkpoint": "mistralai/Mistral-7B-Instruct-v0.3"
     },
     'llama3':{
         "name": "Llama3",
         "checkpoint": "meta-llama/Meta-Llama-3-8B-Instruct"
     },
-    'mistral':{
-        "name": "Mistral",
-        "checkpoint": "mistralai/Mistral-7B-Instruct-v0.3"
+    'llama2':{
+        "name": "Llama2",
+        "checkpoint": "meta-llama/Llama-2-7b-chat-hf"
     }
 }
