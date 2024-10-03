6a4e33113957fdc78a8d16c56a9a318289fc2717
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2024-10-02 18:42:42 -0700

    ENH #10: Add Configuration for LLM model and tokenizer setup

---
+++ src/config/envs.py
@@ -0,0 +1,5 @@
+from src.config import credentials
+
+MODELS_DIR = f'/scratch/{credentials.ASURITE_ID}/cache/llm-sensitivity/models'
+DATASETS_DIR = f'/scratch/{credentials.ASURITE_ID}/cache/llm-sensitivity/datasets'
+MAX_RESPONSE_LEN = 300
--- src/config/models.py
+++ src/config/models.py
@@ -1,10 +1,10 @@
-ModelConfigList = [
-    {
+Config = {
+    'llama3.1':{
         "name": "Llama3.1",
         "checkpoint": "meta-llama/Llama-3.1-8B-Instruct"
     },
-    {
+    'mistral':{
         "name": "Mistral",
         "checkpoint": "mistralai/Mistral-7B-Instruct-v0.3"
     }
-]
+}
