fdae784bb5982d76b1385cbf7b9723181dc8bf87
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2024-10-24 18:12:15 -0700

    ARC #13: Refactor code structure into different component

--- src/scripts/perturbation.py
+++ src/scripts/perturbation.py
@@ -1,6 +1,6 @@
 '''
 -----------------------------------------------------------------------
-File: scripts/augmentors.py
+File: scripts/perturbation.py
 Creation Time: Oct 17th 2024, 7:21 pm
 Author: Saurabh Zinjad
 Developer Email: saurabhzinjad@gmail.com
@@ -8,10 +8,22 @@ Copyright (c) 2023-2024 Saurabh Zinjad. All rights reserved | https://github.com
 -----------------------------------------------------------------------
 '''
 
+import argparse
+import os
+from typing import List
+import pandas as pd
 import time
 import nlpaug.augmenter.char as nac
 import nlpaug.augmenter.word as naw
 import pandas as pd
+import logging
+
+from src.config import envs
+from src.utils import measure_execution_time, get_dataframe
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 def count_differences(str1, str2):
     """Count the number of character differences between two strings."""
@@ -61,55 +73,51 @@ def get_augmenter(level: str='char', perb_type: str='ocr', aug_word_max: int=1, 
 
     return aug
 
-def measure_execution_time(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        execution_time = end_time - start_time
-        func_run_log = f"Function {func.__name__} took {execution_time:.4f} seconds to execute"
-        print(func_run_log)
-        # if 'is_st' in kwargs and kwargs['is_st']:
-        #     st.write(func_run_log)
-
-        return result
-
-    return wrapper
-
 @measure_execution_time
-def augment_ocr_errors_in_questions(df: pd.DataFrame, level: str='char', perb_type: str = 'ocr', max_char_perturb: int=5) -> pd.DataFrame:
+def perturb_questions(dataset_path: str = None, perturb_level: str='char', perturb_type: str = 'ocr', max_char_perturb: int=5, columns: List[str] = None) -> pd.DataFrame:
     """Augment questions in the DataFrame with OCR changes."""
     try:
-        for i in range(1, max_char_perturb + 1):
-            current_col = []
-            current_col_name = f'{perb_type}_n{i}'
-            print(f"{'='*15} Column: {current_col_name} {'='*15}")
+        if dataset_path:
+            df = get_dataframe(dataset_path)
+            if df.empty:
+                raise ValueError("Empty DataFrame. Check dataset path or format.")
+            for col in columns:
+                logger.info(f"{'='*5} Processing column: {col}")
+                query_col = df[col].to_list()
+                query_col_len = len(query_col)
 
-            aug = get_augmenter(level, perb_type)
+                for i in range(1, max_char_perturb + 1):
+                    current_col = []
+                    current_col_name = f'{perturb_type}_n{i}_{col.lower()}'
+                    print(f"{'='*15} Column: {current_col_name} {'='*15}")
 
-            for idx, text in enumerate(df['Question']):
-                if idx == 9:
-                    pass
-                print(f"{idx + 1}/{len(df)}")
-                augmented_text = aug.augment(text)[0]
-                try_flag = 0
-                while count_differences(text, augmented_text) != i and try_flag < 10:
-                    augmented_text = aug.augment(text)[0]
-                    try_flag += 1
-                current_col.append(augmented_text)
+                    aug = get_augmenter(perturb_level, perturb_type)
 
-            df[current_col_name] = current_col
+                    for idx, text in enumerate(query_col):
+                        print(f"{idx + 1}/{query_col_len}")
+                        augmented_text = aug.augment(text)[0]
+                        try_flag = 0
+                        while count_differences(text, augmented_text) != i and try_flag < 10:
+                            augmented_text = aug.augment(text)[0]
+                            try_flag += 1
+                        current_col.append(augmented_text)
 
-        return df
+                    df[current_col_name] = current_col
+
+            return df
     except Exception as e:
         print(f"Error: {e}")
 
 if __name__ == "__main__":
-    df = pd.read_csv('/Users/saurabh/AA/convergent/projects/llm-sensitivity/data/CatHarmQA/CatHarmModelPeturb.csv')
-    df = augment_ocr_errors_in_questions(df, level='char', perb_type='ocr', max_char_perturb=5)
-    df = augment_ocr_errors_in_questions(df, level='char', perb_type='keyboard', max_char_perturb=5)
-    df = augment_ocr_errors_in_questions(df, level='char', perb_type='random_insert', max_char_perturb=5)
-    df = augment_ocr_errors_in_questions(df, level='char', perb_type='random_substitute', max_char_perturb=5)
-    df = augment_ocr_errors_in_questions(df, level='char', perb_type='random_swap', max_char_perturb=5)
-    df = augment_ocr_errors_in_questions(df, level='char', perb_type='random_delete', max_char_perturb=5)
-    df.to_csv('/Users/saurabh/AA/convergent/projects/llm-sensitivity/data/CatHarmQA/CatHarmModelPeturb.csv', index=False)
+    parser = argparse.ArgumentParser(description='Run perturbation experiments.')
+    parser.add_argument('--dataset_path', type=str, default=None, help='Path to the dataset file.')
+    parser.add_argument('--perturbation_level', type=str, choices=['char', 'word', 'sentence'], default='char', help='Level of perturbation to apply.')
+    parser.add_argument('--perturbation_type', type=str, choices=['ocr', 'keyboard', 'random_insert', 'random_substitute', 'random_swap', 'random_delete', 'spelling'], default='ocr', help='Type of perturbation to apply.')
+    parser.add_argument('--max_char_perturb', type=int, default=5, help='Maximum number of character perturbations to make.')
+    parser.add_argument('--columns', type=str, nargs='+', default=None, help='Columns containing query to generate response for.')
+
+    args = parser.parse_args()
+
+    perturbed_df = perturb_questions(args.dataset_path, args.perturbation_level, args.perturbation_type, args.max_char_perturb, args.columns)
+    output_path = os.path.join(envs.PERTURBED_DATA_DIR, f'{args.perturbation_level}_{args.perturbation_type}_{args.max_char_perturb}.csv')
+    perturbed_df.to_csv(output_path, index=False)
