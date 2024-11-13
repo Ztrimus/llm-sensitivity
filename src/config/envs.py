c9e78fddd7df034ce253e59da64c958a15e50b49
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2024-11-12 17:10:24 -0700

    CLN #24: shift the dataset of safety

--- src/config/envs.py
+++ src/config/envs.py
@@ -10,6 +10,9 @@ GENERATED_DATA_DIR = (
     f"/scratch/{credentials.ASURITE_ID}/llm-sensitivity/data/generated/catHarmQA/"
 )
 SAFETY_DATA_DIR = (
-    f"/scratch/{credentials.ASURITE_ID}/llm-sensitivity/data/safety/catHarmQA/"
+    f"/scratch/{credentials.ASURITE_ID}/llm-sensitivity/data/safety/catHarmQA/response"
+)
+SAFETY_QUESTIONS_DATA_DIR = (
+    f"/scratch/{credentials.ASURITE_ID}/llm-sensitivity/data/safety/catHarmQA/response"
 )
 MAX_RESPONSE_LEN = 256
