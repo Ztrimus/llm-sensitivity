c506504c3a00f049058918aa94ea1753d472e9ec
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2024-11-25 04:44:21 -0700

    ENH: Add two model qwen25 and gemma2

--- src/config/models.py
+++ src/config/models.py
@@ -1,18 +1,8 @@
 Config = {
-    'mistral':{
-        "name": "Mistral",
-        "checkpoint": "mistralai/Mistral-7B-Instruct-v0.3"
-    },
-    'llama2':{
-        "name": "Llama2",
-        "checkpoint": "meta-llama/Llama-2-7b-chat-hf"
-    },
-    'llama3':{
-        "name": "Llama3",
-        "checkpoint": "meta-llama/Meta-Llama-3-8B-Instruct"
-    },
-    'llama31':{
-        "name": "Llama31",
-        "checkpoint": "meta-llama/Llama-3.1-8B-Instruct"
-    },
+    "mistral": {"name": "Mistral", "checkpoint": "mistralai/Mistral-7B-Instruct-v0.3"},
+    "llama2": {"name": "Llama2", "checkpoint": "meta-llama/Llama-2-7b-chat-hf"},
+    "llama3": {"name": "Llama3", "checkpoint": "meta-llama/Meta-Llama-3-8B-Instruct"},
+    "llama31": {"name": "Llama31", "checkpoint": "meta-llama/Llama-3.1-8B-Instruct"},
+    "gemma2": {"name": "Gemma2", "checkpoint": "google/gemma-2-9b-it"},
+    "qwen25": {"name": "Qwen2.5", "checkpoint": "Qwen/Qwen2.5-7B-Instruct"},
 }
