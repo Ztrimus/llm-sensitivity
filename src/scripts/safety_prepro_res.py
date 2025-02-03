26c54ab11057b21d09938f57de7df098e0416074
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2025-02-02 19:11:17 -0700

    FIX #26: Handle empty text for generating output from llama-guard

--- src/scripts/safety_prepro_res.py
+++ src/scripts/safety_prepro_res.py
@@ -9,6 +9,7 @@ Copyright (c) 2023-2025 Saurabh Zinjad. All rights reserved | https://github.com
 """
 
 import os
+import argparse
 
 import traceback
 from utils import measure_execution_time
@@ -28,26 +29,36 @@ logger = logging.getLogger(__name__)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 logger.info(f"Device: {device}")
 
+def filter_safety_response(label):
+    return label.strip().split()[0].lower()
 
 @measure_execution_time
 def moderate(model, tokenizer, texts):
     try:
         output_texts = []
         for id, text in enumerate(texts):
-            logger.info(f"{'='*15} {id+1}/{len(texts)} text: {text}")
+            if isinstance(text, str):
+                logger.info(f"{'='*15} {id+1}/{len(texts)} text: {text}")
 
-            chat = [{"role": "user", "content": text}]
-            input_ids = tokenizer.apply_chat_template(chat, return_tensors="pt").to(
-                device
-            )
-            output = model.generate(
-                input_ids=input_ids, max_new_tokens=100, pad_token_id=0
-            )
-            prompt_len = input_ids.shape[-1]
-            output_text = tokenizer.decode(
-                output[0][prompt_len:], skip_special_tokens=True
-            )
-            output_texts.append(output_text)
+                chat = [{"role": "user", "content": text}]
+                input_ids = tokenizer.apply_chat_template(chat, return_tensors="pt").to(
+                    device
+                )
+                output = model.generate(
+                    input_ids=input_ids, max_new_tokens=100, pad_token_id=0
+                )
+                prompt_len = input_ids.shape[-1]
+                output_text = tokenizer.decode(
+                    output[0][prompt_len:], skip_special_tokens=True
+                )
+                try:
+                    output_text = filter_safety_response(output_text)
+                except Exception as e:
+                    logger.error(f"output_text: {output_text}")
+                    logger.error(f"Error filtering response: {str(e)}")
+                output_texts.append(output_text)
+            else:
+                output_texts.append('safe')
         return output_texts
     except Exception as e:
         logger.error(f"Error generating text: {str(e)}")
@@ -63,18 +74,24 @@ def check_safety(dataset_path):
             token=credentials.HF_TOKEN,
             cache_dir=envs.MODELS_DIR,
         )
-
         model = AutoModelForCausalLM.from_pretrained(
             pretrained_model_name_or_path=model_id,
             token=credentials.HF_TOKEN,
             cache_dir=envs.MODELS_DIR,
         ).to(device)
 
-        question_col_list = ["original_response_pre", "perturbed_response_pre"]
+        question_col_list = ["perturbed_response_pre", "original_response_pre"]
         data = pd.read_csv(dataset_path)
 
         for question_col in question_col_list:
             questions = data[question_col].to_list()
+
+            # Test for null values
+            try:
+                empty_texts = data[data[question_col].isnull() == True][question_col].tolist()[0]
+                print(moderate(model, tokenizer, questions))
+            except Exception as e:
+                logger.error(f"Error in null values: {str(e)}")
 
             output_texts = moderate(model, tokenizer, questions)
             print(f"Storing response in dataframe")
